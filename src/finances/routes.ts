103cff36920d47e14b6d0c727e14d6d65852fbff
Adryan <adrianrogowski80@gmail.com>
2023-05-30 22:11:10 +0200

    Fix finance

--- src/finances/routes.ts
+++ src/finances/routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { getStudentFinances, getStudentNearFinance, updateFinance, deleteFinance, createFinance } from "./controller";
+import { getStudentFinances, updateFinance, deleteFinance, createFinance } from "./controller";
 
 const router = Router();
 
 router.get("/:id", getStudentFinances);
-router.get("/near/:id", getStudentNearFinance);
 router.put("/:id", updateFinance);
 router.delete("/:id", deleteFinance);
 router.post("/", createFinance);
