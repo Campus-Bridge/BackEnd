f5c4f38e33808dbdba18457c54c0b32e2f9529b4
Adryan <adrianrogowski80@gmail.com>
2023-07-06 20:42:40 +0200

    middleware: Logs

--- src/middlewares/log.ts
+++ src/middlewares/log.ts
@@ -2,5 +2,4 @@ import { Request, Response, NextFunction } from "express";
 
 export function logger(req: Request, res: Response, next: NextFunction) {
   console.log(`${req.method} ${req.path}`);
-  next();
 }
