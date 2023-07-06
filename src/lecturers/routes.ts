4e12e31523f29b846fede7e1150274de7ec678c1
Adryan <adrianrogowski80@gmail.com>
2023-07-06 23:42:19 +0200

    delete, update endpoints

--- src/lecturers/routes.ts
+++ src/lecturers/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
-import { getLecturers, getLecturerById } from "./controller";
+import { getLecturers, getLecturerById, updateLecturer, deleteLecturer } from "./controller";
 
 const router = Router();
 
 router.get("/", getLecturers);
 router.get("/:id", getLecturerById);
-router.put("/:id");
+router.put("/:id", updateLecturer);
 router.post("/");
+router.delete("/:id", deleteLecturer);
 
 export default router;
